d2e265fea65038517c657cb4325ed4b2875ae5d9
Muhammad Syahrul <48862920+msyahrul801@users.noreply.github.com>
2022-08-19 12:08:16 +0700

    ok

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LOGIN from '../components/compLogin'
+
+const Login = (props) => {
+    return (
+      <>
+        <LOGIN/>
+      </>
+    )
+  }
+  
+  export default Login
